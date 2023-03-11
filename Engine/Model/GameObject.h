646833dd1350463cd1b0c74474033894b090bf7c
Andy <104561961+AndyComeros@users.noreply.github.com>
2023-03-11 09:46:11 +0800

    Scene and GameObject data finalized

--- Engine/Model/GameObject.h
+++ Engine/Model/GameObject.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Graphics/Graphics.h"
 #include <glm/glm.hpp>
+#include <string>
 //#include <glm/gtc/matrix_transform.hpp>
 //#include <glm/gtc/type_ptr.hpp>
 
@@ -14,10 +15,13 @@ public:
 	glm::vec3 scale =	{0,0,0};
 	glm::vec3 rotation = {0,0,0};
 
-
 	Model3D* model_data = nullptr;
 	Shader* shader = nullptr;
+	std::string name;
+
+	int GetID();
 private:
 
 	unsigned int ID;
+	
 };
--- Engine/Model/Scene.h
+++ Engine/Model/Scene.h
@@ -2,13 +2,15 @@
 #include "GameObject.h"
 #include <vector>
 
+
+//plz make singleton
 class Scene
 {
 public:
 	Scene();
 	~Scene();
 
-
+	CubeMap* skybox;
 	std::vector<GameObject> gameObjects;
 private:
 	
