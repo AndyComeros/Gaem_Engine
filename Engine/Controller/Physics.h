15b41dfd68784c7c31034f04c89e44a71fea94db
nathan <natecause22@gmail.com>
2023-04-09 16:46:44 +0800

    facade more functions from react lib

--- Engine/Controller/Physics.h
+++ Engine/Controller/Physics.h
@@ -5,24 +5,82 @@
 
 using namespace reactphysics3d;
 
+// RigidBody type
+#define KINE 1
+#define STAT 2
+#define DYNA 3
+
 class Physics
 {
 public:
 	void CreatePhysicsWorld();
+	void DestroyPhysicsWorld();
+
+	//=========RIGIDBODY============START==
+	//add and del rigidbodies
 	void AddRigidBody();
 	void AddRigidBody(GameObject go);
+	void DelRigidBody(int id);
 
+	//modify rigidbodies
+	void ModRigidBodyType(int id, int rbType);
+	void ModRigidBodyGravity(int id, bool state);	// true == on
+
+	//apply force to rigidbodies
+	void ApplyRigidBodyForce(int id, Vector3 force);
+	void ApplyRigidBodyForce(int id, Vector3 force, Vector3 point);
+	void ApplyRigidBodyTorque(int id, Vector3 torque);
+
+	//add and del rigidbody colliders
+	void AddRigidBodyColliderBox(int id, Vector3 scale);
+	void AddRigidBodyColliderSphere(int id, float radius);
+	void AddRigidBodyColliderCapsule(int id, float radius, float height);
+	//TODO add Model3D to convex function
+	//TODO add Model3D to concave function
+	//TODO heightField shape
+	
+	//modify rigidbody colliders
+	//TODO add collision filtering
+	//TODO add trigger
+	//MAYBE hinge and motor
+
+	//=========RIGIDBODY==============END==
+
+	//add and del collision bodies
+	void AddColBody(GameObject go);
+	void DelColBody(int id);
+
+	//ray casting
+	//TODO add Ray casting
+
+
+	//simulation settings
 	void SetTimeStep(float time);
 	void StepPhysics();
-	void UpdateGO(GameObject go);
+
+	//update data storage
+	void UpdateCbStore();
+	void UpdateGoStore();
+
+	//misc
+	inline glm::vec3 react2glm(const Vector3& vec){return { vec.x, vec.y, vec.z };}
+
+
 private:
 	//physics factory and world
 	PhysicsCommon physicsCommon;	
 	PhysicsWorld* world;
 
-	//rigidbody storage
+	//data storage
 	std::map<int, RigidBody*> rbStore;
+	std::map<int, Collider*> colStore;
+	std::map<int, CollisionBody*> cbStore;
+	std::map<int, GameObject> goStore;
 
 	//updates per second
 	decimal timeStep = 0.0f;
+	float accumulator = 0.0f;
+	float previousFrameTime = 0.0f;
+	float mDeltaTime = 0.0f;
 };
+
