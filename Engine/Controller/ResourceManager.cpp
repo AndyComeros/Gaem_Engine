6b341d3157e25647d9f988060125d4b1eab9b2f7
Andy <104561961+AndyComeros@users.noreply.github.com>
2023-04-11 22:03:36 +0800

    Added error handling to resource manager

--- Engine/Controller/ResourceManager.cpp
+++ Engine/Controller/ResourceManager.cpp
@@ -7,27 +7,84 @@ ResourceManager& ResourceManager::Get() {
 	return r_instance;
 }
 
-ResourceManager::ResourceManager(){}
+ResourceManager::ResourceManager(){
+
+	//load defaults
+}
+
 ResourceManager::~ResourceManager(){}
 
 void ResourceManager::LoadTexture(std::string fileName, std::string resName) {
-	textures.emplace(resName,Texture(fileName.c_str()));
+	try
+	{
+		textures.emplace(resName, Texture(fileName.c_str()));
+	}
+	catch (const std::exception&)
+	{
+		std::cout << "Error: Could not load: " << resName << std::endl;
+	}
 }
 
 void ResourceManager::LoadModel(std::string fileName, std::string resName) {
-	models.emplace(resName, Model(fileName.c_str()));
+	try
+	{
+		models.emplace(resName, Model(fileName.c_str()));
+	}
+	catch (const std::exception&)
+	{
+		std::cout << "Error: Could not load: " << resName << std::endl;
+	}
 }
 
 void ResourceManager::LoadShader(std::string vertPath, std::string fragPath, std::string geomPath, std::string resName) {
-	shaders.emplace(resName, Shader(vertPath.c_str(),fragPath.c_str(),geomPath.c_str()));
+	try
+	{
+		shaders.emplace(resName, Shader(vertPath.c_str(), fragPath.c_str(), geomPath.c_str()));
+	}
+	catch (const std::exception&)
+	{
+		std::cout << "Error: Could not load: " << resName << std::endl;
+	}
 }
 
 Texture* ResourceManager::GetTexture(std::string resName) {
-	return &textures.at(resName);
+	Texture* texture;
+	try
+	{
+		texture = &textures.at(resName);
+	}
+	catch (const std::exception&)
+	{
+		std::cout << "Texture: '" << resName << "' does not exist" << std::endl;
+		texture = nullptr;
+	}
+	return texture;
 }
+
 Model* ResourceManager::GetModel(std::string resName) {
-	return &models.at(resName);
+	Model* model;
+	try
+	{
+		model = &models.at(resName);
+	}
+	catch (const std::exception&)
+	{
+		std::cout << "Model: '" << resName << "' does not exist" << std::endl;
+		model = nullptr;
+	}
+	return model;
 }
+
 Shader* ResourceManager::GetShader(std::string resName) {
-	return &shaders.at(resName);
+	Shader* shader;
+	try
+	{
+		shader = &shaders.at(resName);
+	}
+	catch (const std::exception&)
+	{
+		std::cout << "Shader: '" << resName << "' does not exist" << std::endl;
+		shader = nullptr;
+	}
+	return shader;
 }
