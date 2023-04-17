f81080ea5eadfda607beec6857c35d40bc3d7ac7
Andy <104561961+AndyComeros@users.noreply.github.com>
2023-04-17 10:26:21 +0800

    Added function declarations for rigidbody

--- engine/Model/Rigidbody.h
+++ engine/Model/Rigidbody.h
@@ -4,6 +4,16 @@
 class Rigidbody
 {
 public:
+	void ApplyForce(glm::vec3 force);
+
+	void AddColliderBox(Vector3 scale);
+	void AddColliderSphere(float radius);
+	void AddColliderCapsule(float radius, float height);
+
+	void SetUseGravity(bool isGravity);
+	void SetPosition(glm::vec3 newPos);
+
+	void AddColliderHeightMap(Terrain& terrain);
 
 	friend class Physics;
 private:
