38d481ea3450204f71c7044b64e70b22542ec61b
Andy <104561961+AndyComeros@users.noreply.github.com>
2023-04-20 17:56:19 +0800

    more comments

--- Engine/View/GUIRenderer.h
+++ Engine/View/GUIRenderer.h
@@ -6,26 +6,65 @@
 #include <ResourceManager.h>
 #include <map>
 #include <string>
-
-
+/**
+*	@Class GUIRenderer
+*	@brief Used to draw gui elements to the screen
+*
+*	@author Andres Comeros-Ochtman
+*	@version 1.0
+*	@date 15/04/2023
+*/
 class GUIRenderer
 {
 public:
+		/**
+		*	@brief default constructor
+		*/
 	GUIRenderer();
+
+		/**
+		*	@brief destructor
+		*/
 	~GUIRenderer();
+
+		/**
+		*	@brief initalize gui
+		*	@param nwindow window being rendered to
+		*	@return void
+		*/
 	void Init(GLFWwindow* nwindow);
+
+		/**
+		*	@brief Draw current GUI to the screen
+		*	@return void
+		*/
 	void Draw();
 
+		/**
+		*	@brief Switch the current GUI
+		*	@param nGUI GUI being switched to
+		*	@return void
+		*/
 	void SwitchMenu(int nGUI);
 
 private:
-	void GUI_Main();
-	void GUI_Settings();
-	void GUI_Exit();
-	void GUI_Debug();
-	void GUI_InGame();
 
+		///Draw generic main menu
+	void GUI_Main();
+		///Draw generic settings menu
+	void GUI_Settings();
+		///Draw generic exit menu
+	void GUI_Exit();
+		///Draw generic debug menu
+	void GUI_Debug();
+		///Draw generic in game menu
+	void GUI_InGame();
+		
+		///Current GUI to be drawn
 	int currentGUI = 0;
+		
+		///imgui settings for windos
 	ImGuiWindowFlags flags;
+		///window being drawn to
 	GLFWwindow* window;
 };
--- engine/View/Renderer.h
+++ engine/View/Renderer.h
@@ -1,27 +1,84 @@
 #pragma once
 #include <Scene.h>
 #include <Graphics/Graphics.h>
-
+/**
+*	@Class Renderer
+*	@brief Used to draw scene to the screen
+*
+*	@author Andres Comeros-Ochtman
+*	@version 1.0
+*	@date 15/04/2023
+*/
 class Renderer
 {
 public:
-	
+		/**
+		*	@brief default constructor
+		*/
 	Renderer();
+
+		/**
+		*	@brief destructor
+		*/
 	~Renderer();
+
+		/**
+		*	@brief initalize OpenGL
+		*	@param window window being drawn
+		*	@return void
+		*/
 	void Init(GLFWwindow* window);
 
+		/**
+		*	@brief Draw the scene
+		*	@param scene scene to be drawn
+		*	@return void
+		*/
 	void Draw(Scene& scene);
 
+		/**
+		*	@brief Set uniforms for All lights in a scene
+		*	@param sLights lights to set uniforms for
+		*	@param sShader shader to set uniforms for
+		*	@return void
+		*/
 	static void SetLightUniforms(Lights& sLights,Shader& sShader);
+
+		/**
+		*	@brief Set uniforms for all  point lights in a scene
+		*	@param sLights lights to set uniforms for
+		*	@param sShader shader to set uniforms for
+		*	@return void
+		*/
 	static void SetPointLightUniforms(Lights& sLights, Shader& sShader);
+
+		/**
+		*	@brief Set uniforms for all direction lights in a scene
+		*	@param sLights lights to set uniforms for
+		*	@param sShader shader to set uniforms for
+		*	@return void
+		*/
 	static void SetDirectionLightUniforms(Lights& sLights, Shader& sShader);
+
+		/**
+		*	@brief Set uniforms for all spot lights in a scene
+		*	@param sLights lights to set uniforms for
+		*	@param sShader shader to set uniforms for
+		*	@return void
+		*/
 	static void SetSpotLightUniforms(Lights& sLights, Shader& sShader);
 
+		/**
+		*	@brief Returns the default shader
+		*	@return void
+		*/
 	Shader& GetShader();
-
+		///Whether to draw in wireframe mode or not
 	bool wireFrame = false;
 private:
+		///main window
 	GLFWwindow* mainWindow;
+		///default shader
 	Shader mainShader;
 
 };
