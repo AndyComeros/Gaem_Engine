e5fb17a02f66c03457cf35fc7eaf4b19e4c4bc50
MechaHex <natecause22@gmail.com>
2023-05-12 15:41:01 +0800

    experimental physic step tweak

--- resources/scripts/main.lua
+++ resources/scripts/main.lua
@@ -52,7 +52,7 @@ function init()
 
 	--Setup Player
 	Player = resources:CreateGameObject("Player", "AE86", "");
-	Player.position = vec3:new(0,5,0);
+	Player.position = vec3:new(0,35,0);
 	physics:AddRigidBody(Player,3);
 	local scale = vec3:new(1.5,0.2,0.7)
 	local mass = 500;
