0242483e069321e388ec7fcdf13dd269cdfa5a54
Andy <104561961+AndyComeros@users.noreply.github.com>
2023-05-19 14:17:40 +0800

    fixed terrain shader stictching artifact error

--- engine/Model/Terrain.cpp
+++ engine/Model/Terrain.cpp
@@ -143,8 +143,16 @@ void Terrain::SetTextures(std::vector<Texture*> textures, Texture* detailMap) {
 
 void Terrain::SetTextureHeights(std::vector<float> heights) {
 
-	shader->SetUniform("heights", heights);
-	shader->SetUniform("heightCount", (int)heights.size());
+	std::vector<float> nHeights;
+
+	nHeights.push_back(heights[0]);
+	for (int i = 0; i < heights.size(); i++)
+	{
+		nHeights.push_back(heights[i]);
+	}
+
+	shader->SetUniform("heights", nHeights);
+	shader->SetUniform("heightCount", (int)nHeights.size());
 }
 
 
@@ -161,9 +169,10 @@ void Terrain::SetTextureScale(float nScale) {
 
 void Terrain::SetUniforms()
 {
-	if(shader)
+	if (shader) {
 		shader->SetUniform("textures", tUnits);
 		shader->SetUniform("textureCount", tCount);
+	}
 }
 
 void Terrain::LoadHeightMap(const std::string& fileName) {
