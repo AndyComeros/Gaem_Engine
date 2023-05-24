d1d41cdbb5d321fb76536ab0b249a5f9d691a76a
Andy <104561961+AndyComeros@users.noreply.github.com>
2023-05-24 16:30:16 +0800

    Basic LUA menu script

--- resources/scripts/main.lua
+++ resources/scripts/main.lua
@@ -1,15 +1,9 @@
 --dofile("resources/scripts/inputs.lua")
+dofile("resources/scripts/menu.lua")
 
 function init()
 	print("init lua");
-	
-	 saves = loader:GetSaves("resources/saves");
 
-	    for i, v in ipairs(saves) do
-            print(i, v)
-        end
-
-	 print(saves);
 
 	resources:LoadTexture("grass","resources/textures/terrain/grass.png");
 	print("End Init");
@@ -17,20 +11,9 @@ end
 
 function update(deltaTime)
 	
-	GUI:Start(false);
-	--GUI:Tab(500);
-	--GUIRenderer::Image(const std::string texture, float width, float height, float alignX, float alignY)
-		
-	GUI:Text("Hello world!",0.5);
-	--GUI:SetCursorPosY(0);
-	--Button(const std::string label, float alignment, float width, float height)
 
-	if (GUI:Image("grass", 200, 200, 0.5, 0.5))
-	then
-		print("press!");
-	end
-
-	GUI:End();
+	draw_menu();
+	
 
 end
 
---
+++ resources/scripts/menu.lua
@@ -0,0 +1,102 @@
+
+
+local cases = {
+	[1] = function()
+		draw_main_menu();
+	end,
+	[2] = function()
+		draw_save_menu();
+	end,
+	[3] = function()
+		draw_options_menu();
+	end,
+	[4] = function()
+		draw_ingame();
+	end
+}
+
+local current_menu = 1;
+
+
+function draw_menu()
+
+	GUI:Start(false);
+	
+	if(cases[current_menu])
+	then
+		cases[current_menu]();
+	end;
+	GUI:End();
+end
+
+
+function draw_main_menu()
+	GUI:Tab(300);
+	GUI:Text("Main Menu",0.5);
+
+	GUI:Tab(10);
+
+	if(GUI:Button("Start Game",0.5, 200, 40))
+	then
+		current_menu = 4;
+	end
+
+	if(GUI:Button("Load Game",0.5, 200, 40))
+	then
+		current_menu = 2;
+	end
+
+	if(GUI:Button("Options",0.5, 200, 40))
+	then
+		current_menu = 3;
+	end
+
+	if(GUI:Button("Exit",0.5, 200, 40))
+	then
+		print("Exit");
+	end
+end
+
+
+function draw_save_menu()
+	
+	GUI:Tab(300);
+	GUI:Text("Choose a save to load:",0.5);
+	GUI:Tab(10);
+
+	if(GUI:Button("Save Current Game",0.5, 200, 40))
+	then
+		print("Save!");
+	end
+
+	saves = loader:GetSaves("resources/saves");
+	for i, v in ipairs(saves)
+	do
+		if(GUI:Button(v, 0.5, 200, 40))
+		then
+			print(v);
+		end
+    end
+	GUI:Tab(30);
+
+	if(GUI:Button("Back",0.5, 200, 40))
+	then
+		current_menu = 1;
+	end
+
+end
+
+function draw_options_menu()
+
+	GUI:Tab(300);
+	GUI:Text("Options",0.5);
+
+	if(GUI:Button("Back",0.5, 200, 40))
+	then
+		current_menu = 1;
+	end
+end
+
+function draw_ingame()
+	GUI:Text("in-game",0.5);
+end
